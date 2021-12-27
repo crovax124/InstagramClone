03c8d589398ed5d766fb414bb490304d7b927166
Eric <eric.isensee11@gmail.com>
2021-12-27 15:09:02 +0100

    Added FireBase

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 A Clone of Instagram for practice by Eric
 
 
+To make this run, create your firebase firestore database and register iOS and Android app and put in following files.
+
+ios/Runner/GoogleService-Info.plist
+android/app/google-services.json
+
 ## Getting Started
 "# InstagramClone" 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import './responsive/web_screen_layout.dart';
 import './responsive/mobile_screen_layout.dart';
-import './utils/colors.dart';
 import './responsive/responsive_layout_screen.dart';
+import './utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+
   runApp(const MyApp());
 }
 
